dbbb2fa064ee22ec8998d1a3c10633916a0f8896
mzhaim11 <mz2548@nyu.edu>
2021-09-11 13:51:51 +0400

    vv
    ddd

--- solution.py
+++ solution.py
@@ -7,9 +7,9 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     global answer
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = mTLS
+        answer = "mTLS"
     elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
+        answer = "yes"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
