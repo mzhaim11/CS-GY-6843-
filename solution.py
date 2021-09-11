cb3a692f8ace0a511dadfd3088325e80fb4a5fbc
mzhaim11 <mz2548@nyu.edu>
2021-09-11 13:45:08 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,7 @@
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
+    global answer
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
     elif question == "Are encoding and encryption the same? - Yes/No":
@@ -25,7 +26,7 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
-    return answer
+    return (answer)
 # Complete all the questions.
 
 
